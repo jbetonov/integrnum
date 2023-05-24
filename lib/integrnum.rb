cbf63e8d16ee7cad44817012f2283bbf07d30a06
jbetonov <torportatzi@gmail.com>
2023-05-24 12:52:14 +0300

    First commt

---
+++ lib/integrnum.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "integrnum/version"
+
+module Integrnum
+  class Error < StandardError; end
+
+  def self.hello
+    'hello world'
+  end
+end
---
+++ lib/integrnum/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Integrnum
+  VERSION = "0.1.0"
+end
