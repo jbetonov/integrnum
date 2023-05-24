9bf393bb1d809db742f49004805ab2feebd2012a
712meeral <mrbetaylas@gmail.com>
2023-05-24 13:05:20 +0300

    Delete tests

--- test/test_integrnum.rb
+++ test/test_integrnum.rb
@@ -3,11 +3,5 @@
 require "test_helper"
 
 class TestIntegrnum < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Integrnum::VERSION
-  end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
