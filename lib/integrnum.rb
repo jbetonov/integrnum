3ab76b000e2ac064e5e430bafbb4ea74e37092e6
jbetonov <torportatzi@gmail.com>
2023-05-29 22:54:03 +0300

    Add functions stubs

--- lib/integrnum.rb
+++ lib/integrnum.rb
@@ -8,4 +8,16 @@ module Integrnum
   def self.hello
     'hello world'
   end
+
+  def self.rectangular(min_lim, max_lim, n)
+
+  end
+
+  def self.trapezoid(min_lim, max_lim, delta)
+
+  end
+
+  def self.parabola(min_lim, max_lim, delta)
+  end
+
 end
--- lib/integrnum/version.rb
+++ lib/integrnum/version.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 module Integrnum
+<<<<<<< Updated upstream
   VERSION = "1.0.0"
+=======
+  VERSION = "0.1.1"
+>>>>>>> Stashed changes
 end
