66d8264c5e95b4124388cba7e974d40dbd5054a3
jbetonov <torportatzi@gmail.com>
2023-05-31 12:05:34 +0300

    Add returns

--- lib/integrnum.rb
+++ lib/integrnum.rb
@@ -10,14 +10,15 @@ module Integrnum
   end
 
   def self.rectangular(min_lim, max_lim, n)
-
+    return 0
   end
 
   def self.trapezoid(min_lim, max_lim, delta)
-
+    return 0
   end
 
   def self.parabola(min_lim, max_lim, delta)
+    return 0
   end
 
 end
