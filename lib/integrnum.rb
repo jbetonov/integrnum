c6780530964443f2887ddd8b744a296d5080fe6c
712meeral <mrbetaylas@gmail.com>
2023-05-31 12:41:54 +0300

    update tests

--- lib/integrnum.rb
+++ lib/integrnum.rb
@@ -6,15 +6,16 @@ module Integrnum
   class Error < StandardError; end
 
 
-  def self.rectangular(min_lim, max_lim, n)
-
+  def self.rectangular(min_lim, max_lim, n=1000, &block)
+    return 0
   end
 
   def self.trapezoid(min_lim, max_lim, delta)
-
+    return 0
   end
 
   def self.parabola(min_lim, max_lim, delta)
+    return 0
   end
 
 end
