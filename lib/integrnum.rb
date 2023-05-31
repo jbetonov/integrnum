fd7f9996d3fa34f3e8e63a14041c8324386760ae
jbetonov <torportatzi@gmail.com>
2023-05-31 13:19:53 +0300

    Update rect method

--- lib/integrnum.rb
+++ lib/integrnum.rb
@@ -2,29 +2,46 @@
 
 require_relative "integrnum/version"
 
+  require 'matrix'
+
 module Integrnum
   class Error < StandardError; end
 
-def func(x)
-return ( 1 / (1+ x * x))
-end
 
-  def self.rectangular(func, min_lim, max_lim, n = 10) # fun через yield
-    h = (max_lim - min_lim ) / n
-    sum = 0
-    for i in 0..n do
-      s += h * func.(min_lim + h * (i + 0.5))
-end
-    return s
-end
-
-
-  def self.trapezoid(min_lim, max_lim, delta)
-    return 0
+  def get_i
+    Math.exp(1) - Math.exp(0)
   end
 
-  def self.parabola(min_lim, max_lim, delta)
-    return 0
+  def self.rectangular(func, min_lim, max_lim, delta)
+    def self.integrate(func, min_lim, max_lim, n)
+      integral = 0.0
+      step = (max_lim - min_lim) / n
+      (min_lim...max_lim-step).step(step) do |x|
+        integral += step * func.call(x + step / 2)
+      end
+      integral
+    end
+
+    d, n = 1, 1
+    while d.abs > delta
+      d = (integrate(func, min_lim, max_lim, n * 2) - integrate(func, min_lim, max_lim, n)) / 3
+      n *= 2
+    end
+
+    a = integrate(func, min_lim, max_lim, n).abs
+    b = integrate(func, min_lim, max_lim, n).abs + d
+    a, b = b, a if a > b
+    puts 'Rectangles:'
+    puts "\t#{n}\t#{a}\t#{b}"
   end
+
+
+def self.trapezoid(min_lim, max_lim, delta)
+    return 0
+end
+
+def self.parabola(min_lim, max_lim, delta)
+    return 0
+end
 
 end
