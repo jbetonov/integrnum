957798f5b020d4a8462ffc91b682cb7ae76dc15d
jbetonov <torportatzi@gmail.com>
2023-05-31 14:02:40 +0300

    Update trapezoid and parabola funcs

--- lib/integrnum.rb
+++ lib/integrnum.rb
@@ -12,6 +12,8 @@ module Integrnum
     Math.exp(1) - Math.exp(0)
   end
 
+#метод прямоугольников
+#pectangular(lambda { |x| Math.sin(x) }, 0, Math::PI, 0.0001)
   def self.rectangular(func, min_lim, max_lim, delta)
     def self.integrate(func, min_lim, max_lim, n)
       integral = 0.0
@@ -36,12 +38,55 @@ module Integrnum
   end
 
 
-def self.trapezoid(min_lim, max_lim, delta)
-    return 0
+#метод трапеций
+def self.trapezoid(func, min_lim, max_lim, delta)
+    def self.integrate(func, min_lim, max_lim, n)
+        integral = 0.0
+        step = (max_lim - min_lim) / n
+        (min_lim...max_lim-step).step(step) do |x|
+            integral += step*(func.call(x) + func.call(x + step)) / 2
+        end
+        integral
+      end
+
+    d, n = 1, 1
+    while d.abs > delta
+        d = (integrate(func, min_lim, max_lim, n * 2) - integrate(func, min_lim, max_lim, n)) / 3
+        n *= 2
+      end
+
+    a = integrate(func, min_lim, max_lim, n).abs
+    b = integrate(func, min_lim, max_lim, n).abs + d
+        a, b = b, a if a > b
+    puts 'Trapezium:'
+    puts "\t#{n}\t#{a}\t#{b}"
 end
 
-def self.parabola(min_lim, max_lim, delta)
-    return 0
+#метод парабол
+
+
+def self.parabola(func, min_lim, max_lim, delta)
+    def self.integrate(func, min_lim, max_lim, n)
+        integral = 0.0
+        step = (max_lim - min_lim) / n
+        (min_lim + step / 2..max_lim - step / 2).step(step) do |x|
+            integral += step / 6 * (func.call(x - step / 2) + 4 * func.call(x) + func.call(x + step / 2))
+        end
+integral
+end
+
+    d, n = 1, 1
+    while d.abs > delta
+        d = (integrate(func, min_lim, max_lim, n * 2) - integrate(func, min_lim, max_lim, n)) / 15
+        n *= 2
+      end
+
+    a = (integrate(func, min_lim, max_lim, n)).abs
+    b = (integrate(func, min_lim, max_lim, n)).abs + d
+
+        a, b = b, a if a>b
+    puts 'Parabola:'
+    puts "\t#{n}\t#{a}\t#{b}"
 end
 
 end
