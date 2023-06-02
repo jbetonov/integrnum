f31aa11e8eb258e28b65b5d4d0fe8e35ae92de1a
712meeral <mrbetaylas@gmail.com>
2023-06-02 15:23:41 +0300

    update tests and Gemfile.lock

---
+++ lib/integrnum_BACKUP_1069.rb
@@ -0,0 +1,136 @@
+# frozen_string_literal: true
+
+require_relative "integrnum/version"
+
+# require 'matrix'
+
+module Integrnum
+  class Error < StandardError; end
+
+
+  def get_i
+    Math.exp(1) - Math.exp(0)
+  end
+
+  #метод прямоугольников
+  #pectangular(lambda { |x| Math.sin(x) }, 0, Math::PI, 0.0001)
+  def self.rectangular(func, min_lim, max_lim, delta)
+    def self.integrate(func, min_lim, max_lim, n)
+      integral = 0.0
+      step = (max_lim - min_lim) / n.to_f
+      (min_lim...max_lim-step).step(step) do |x|
+        integral += step * func.call(x + step / 2)
+      end
+      integral
+    end
+
+    d, n = 1, 1
+    while d.abs > delta
+      d = (integrate(func, min_lim, max_lim, n * 2) - integrate(func, min_lim, max_lim, n)) / 3
+      n *= 2
+    end
+
+    a = integrate(func, min_lim, max_lim, n).abs
+    b = integrate(func, min_lim, max_lim, n).abs + d
+    a, b = b, a if a > b
+
+    #return a
+    return [n, a, b]
+  end
+
+
+<<<<<<< HEAD
+  #метод трапеций
+  def self.trapezoid(func, min_lim, max_lim, delta)
+    def self.integrate(func, min_lim, max_lim, n)
+      integral = 0.0
+      step = (max_lim - min_lim) / n
+      (min_lim...max_lim-step).step(step) do |x|
+        integral += step*(func.call(x) + func.call(x + step)) / 2
+      end
+      integral
+=======
+#метод трапеций
+  def self.trapezoid(func, min_lim, max_lim, delta)
+    def self.integrate(func, min_lim, max_lim, n)
+        integral = 0.0
+        step = (max_lim - min_lim) / n.to_f
+        (min_lim...max_lim-step).step(step) do |x|
+            integral += step*(func.call(x) + func.call(x + step)) / 2
+        end
+        integral
+>>>>>>> 442e1e590fd83150c4b903368520626e7be87245
+    end
+
+    d, n = 1, 1
+    while d.abs > delta
+<<<<<<< HEAD
+      d = (integrate(func, min_lim, max_lim, n * 2) - integrate(func, min_lim, max_lim, n)) / 3
+      n *= 2
+=======
+        d = (integrate(func, min_lim, max_lim, n * 2) - integrate(func, min_lim, max_lim, n)) / 3
+        n *= 2
+>>>>>>> 442e1e590fd83150c4b903368520626e7be87245
+    end
+
+    a = integrate(func, min_lim, max_lim, n).abs
+    b = integrate(func, min_lim, max_lim, n).abs + d
+    a, b = b, a if a > b
+<<<<<<< HEAD
+    puts 'Trapezium:'
+    puts "\t#{n}\t#{a}\t#{b}"
+  end
+
+  #метод парабол
+  def self.parabola(func, min_lim, max_lim, delta)
+    def self.integrate(func, min_lim, max_lim, n)
+      integral = 0.0
+      step = (max_lim - min_lim) / n
+      (min_lim + step / 2..max_lim - step / 2).step(step) do |x|
+        integral += step / 6 * (func.call(x - step / 2) + 4 * func.call(x) + func.call(x + step / 2))
+      end
+      integral
+=======
+
+    return [n, a, b]
+end
+
+#метод парабол
+  def self.parabola(func, min_lim, max_lim, delta)
+    def self.integrate(func, min_lim, max_lim, n)
+        integral = 0.0
+        step = (max_lim - min_lim) / n.to_f
+        (min_lim + step / 2..max_lim - step / 2).step(step) do |x|
+            integral += step / 6 * (func.call(x - step / 2) + 4 * func.call(x) + func.call(x + step / 2))
+        end
+        integral
+>>>>>>> 442e1e590fd83150c4b903368520626e7be87245
+    end
+
+    d, n = 1, 1
+    while d.abs > delta
+<<<<<<< HEAD
+      d = (integrate(func, min_lim, max_lim, n * 2) - integrate(func, min_lim, max_lim, n)) / 15
+      n *= 2
+=======
+        d = (integrate(func, min_lim, max_lim, n * 2) - integrate(func, min_lim, max_lim, n)) / 15
+        n *= 2
+>>>>>>> 442e1e590fd83150c4b903368520626e7be87245
+    end
+
+    a = (integrate(func, min_lim, max_lim, n)).abs
+    b = (integrate(func, min_lim, max_lim, n)).abs + d
+
+    a, b = b, a if a>b
+<<<<<<< HEAD
+    puts 'Parabola:'
+    puts "\t#{n}\t#{a}\t#{b}"
+  end
+end
+=======
+
+    return [n, a, b]
+end
+
+end
+>>>>>>> 442e1e590fd83150c4b903368520626e7be87245
