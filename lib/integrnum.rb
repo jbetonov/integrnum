442e1e590fd83150c4b903368520626e7be87245
jbetonov <torportatzi@gmail.com>
2023-06-02 11:11:34 +0300

    Fix functions, add proper output

--- lib/integrnum.rb
+++ lib/integrnum.rb
@@ -17,7 +17,7 @@ module Integrnum
   def self.rectangular(func, min_lim, max_lim, delta)
     def self.integrate(func, min_lim, max_lim, n)
       integral = 0.0
-      step = (max_lim - min_lim) / n
+      step = (max_lim - min_lim) / n.to_f
       (min_lim...max_lim-step).step(step) do |x|
         integral += step * func.call(x + step / 2)
       end
@@ -33,58 +33,59 @@ module Integrnum
     a = integrate(func, min_lim, max_lim, n).abs
     b = integrate(func, min_lim, max_lim, n).abs + d
     a, b = b, a if a > b
-    puts 'Rectangles:'
-    puts "\t#{n}\t#{a}\t#{b}"
+
+    #return a
+    return [n, a, b]
   end
 
 
 #метод трапеций
-def self.trapezoid(func, min_lim, max_lim, delta)
+  def self.trapezoid(func, min_lim, max_lim, delta)
     def self.integrate(func, min_lim, max_lim, n)
         integral = 0.0
-        step = (max_lim - min_lim) / n
+        step = (max_lim - min_lim) / n.to_f
         (min_lim...max_lim-step).step(step) do |x|
             integral += step*(func.call(x) + func.call(x + step)) / 2
         end
         integral
-      end
+    end
 
     d, n = 1, 1
     while d.abs > delta
         d = (integrate(func, min_lim, max_lim, n * 2) - integrate(func, min_lim, max_lim, n)) / 3
         n *= 2
-      end
+    end
 
     a = integrate(func, min_lim, max_lim, n).abs
     b = integrate(func, min_lim, max_lim, n).abs + d
-        a, b = b, a if a > b
-    puts 'Trapezium:'
-    puts "\t#{n}\t#{a}\t#{b}"
+    a, b = b, a if a > b
+
+    return [n, a, b]
 end
 
 #метод парабол
-def self.parabola(func, min_lim, max_lim, delta)
+  def self.parabola(func, min_lim, max_lim, delta)
     def self.integrate(func, min_lim, max_lim, n)
         integral = 0.0
-        step = (max_lim - min_lim) / n
+        step = (max_lim - min_lim) / n.to_f
         (min_lim + step / 2..max_lim - step / 2).step(step) do |x|
             integral += step / 6 * (func.call(x - step / 2) + 4 * func.call(x) + func.call(x + step / 2))
         end
-integral
-end
+        integral
+    end
 
     d, n = 1, 1
     while d.abs > delta
         d = (integrate(func, min_lim, max_lim, n * 2) - integrate(func, min_lim, max_lim, n)) / 15
         n *= 2
-      end
+    end
 
     a = (integrate(func, min_lim, max_lim, n)).abs
     b = (integrate(func, min_lim, max_lim, n)).abs + d
 
-        a, b = b, a if a>b
-    puts 'Parabola:'
-    puts "\t#{n}\t#{a}\t#{b}"
+    a, b = b, a if a>b
+
+    return [n, a, b]
 end
 
 end
